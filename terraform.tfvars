82a2ca3b651d41386d8a0ef06633c6706eaf3680
Declan Gowran <Declan.Gowran@leaseplan.com>
2021-03-14 19:25:30 +0000

    cluster update

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+appId    = "e5cd6490-8352-4669-900b-da18a6a8bd13"
+password = "Pj1T0f0c5LnlctUU_HIYas_otSV97AcHRy"
--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.34"
+      version = "2.42.0"
     }
   }
 
-  required_version = ">= 0.12"
+  required_version = "~> 0.14"
 }
+
